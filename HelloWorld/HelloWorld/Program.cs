dea486871a8d580f36acdfbb8894ea7dcf6a2cac
jorgemhtdev <jorge@jorgemht.dev>
2018-06-10 17:51:17 +0200

    Initial Commit. Basic Test passed

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld
+{
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World" +"\nPress enter to close...");
+            Console.WriteLine("");
+            Console.ReadLine();
+        }
+    }
+}
